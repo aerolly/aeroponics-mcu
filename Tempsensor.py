09f9095678f4fafb16f8701138e222adee1f3058
zjlin <zijunlin32@gmail.com>
2020-08-15 12:41:56 -1000

    Update Tempsensor.py

--- Tempsensor.py
+++ Tempsensor.py
@@ -2,12 +2,11 @@ import time
 import board 
 import busio
 i2c = busio.I2C(board.SCL, board.SDA) 
-import adafruit_ads1x15.ads1115 as ads 
+import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 ads = ADS.ADS1115(i2c)
-chan = AnalogIn(ads, ADS.P0)
-print(chan.value, chan.voltage)
 
+# port is in the form of p0 p1 p2 p3
 def printval(port):
     chan = AnalogIn(ads, ADS.port)
     print(chan.value, chan.voltage)
