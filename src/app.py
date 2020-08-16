b70dd8b86b249170a85aa3beb8aa69af2dcf7122
Spencer Young <spjy@hawaii.edu>
2020-08-16 07:43:35 -1000

    fix: import

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import simplejson as json
 
 import settings
 from commands import Command
-from temperature import initializeTemperature
+from sensors.temperature import initializeTemperature
 
 r = redis.Redis(host=os.getenv('REDIS_SERVER'), port=os.getenv('REDIS_PORT'), db=0)
 p = r.pubsub(ignore_subscribe_messages=True)
