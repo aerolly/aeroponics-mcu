6d1780dd364ec715fedf33b0f8492483c800c9d9
Spencer Young <spjy@hawaii.edu>
2020-08-22 15:25:12 -1000

    feat: add  pressure sensor

--- src/sensors/pressure.py
+++ src/sensors/pressure.py
@@ -1,2 +1,16 @@
+import time
+import board
+import busio
+import time
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+ads = ADS.ADS1115(i2c)
+
+# port is in the form of p0 p1 p2 p3
 def run():
-  print('test')
+    chan = AnalogIn(ads, ADS.P0)
+    return chan.voltage * (150 / 5)
