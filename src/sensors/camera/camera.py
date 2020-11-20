90e01c633d4b230b2eb0f029a7f15cbf1647c5e1
Mastanese <kl26@hawaii.edu>
2020-11-19 15:17:47 -1000

    feat: camera as sensor

---
+++ src/sensors/camera/camera.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+import sys
+import time
+import subprocess
+
+
+def run(param):
+    currentDateTime = datetime.datetime.now().strftime("%Y-%m-%d_%H%M")
+
+    destUser = os.getenv('SERVER_USER')
+    imageName = f'{param}_{currentDateTime}.png'
+    localDestPath = f'/home/{destUser}/aeroponics-mcu/src/sensors/images/{imageName}'
+    remoteDestPath = f'/home/{destUser}/images/'
+    cameraPath = f'/dev/{param}'
+
+    takePic = f'fswebcam -d {cameraPath} -r 1280x720 --no-banner {localDestPath}'
+
+    os.system(takePic)
+
+    os.system(f'scp {localDestPath} {destUser}@{remoteDestPath}')
